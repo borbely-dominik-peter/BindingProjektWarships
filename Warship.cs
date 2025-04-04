28669a6b654db6b8cf947582c2961c1f8d3e34ea
borbely-dominik-peter <borbely.dominik.peter@students.jedlik.eu>
2025-04-04 13:28:01 +0200

    Test First commit

---
+++ Warship.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingProjektWarships
+{
+    public class Warship
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Type { get; set; }
+        public int Launched { get; set; }
+        public string MainGunCaliber { get; set; }
+        public string Country { get; set; }
+    }
+}
